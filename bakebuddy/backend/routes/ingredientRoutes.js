df357f2c9027832846f7a27f8494b83ce0d2ed0e
ishan sashith rukshan pinto <127678071+it22610102@users.noreply.github.com>
2025-03-15 22:43:59 +0530

    Initialize backend for item and ingredient management

---
+++ bakebuddy/backend/routes/ingredientRoutes.js
@@ -0,0 +1,23 @@
+import express from "express"
+import { 
+    addIngredient, 
+    getIngredients, 
+    getIngredientById, 
+    updateDailyConsumption,
+    updateIngredient,
+    deleteIngredient,
+    getIngredientUsageByIdAndDateRange
+} from "../controllers/ingredientController.js";
+
+const router = express.Router();
+
+router.route("/").get(getIngredients);
+router.route("/add").post(addIngredient);
+router.route("/:id").get(getIngredientById);
+router.route("/:id/daily").put(updateDailyConsumption);
+router.route("/:id").put(updateIngredient);
+router.route("/:id").delete(deleteIngredient);
+router.route("/:id/usage").get(getIngredientUsageByIdAndDateRange);
+
+
+export default router;
