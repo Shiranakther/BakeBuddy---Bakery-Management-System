a34a41f7c9ec1e5befa0bc08041255a44a49bf5e
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2025-03-17 21:59:31 +0530

    create sales jsx page create

--- bakebuddy/frontend/src/App.jsx
+++ bakebuddy/frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import '../css/App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import SalesCreate from './pages/SalesCreate';
 
 
 function App() {
 
 
-  return (
-    <>
-      
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+        <Route path='/create-sales' element={<SalesCreate/>}/>
+    </Routes>
+    </BrowserRouter>
 }
 
 export default App
