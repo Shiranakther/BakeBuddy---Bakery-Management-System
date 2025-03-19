4d4455bc726a1079e1b2183c06c25456deb190e6
shiranshinu <127624730+Shiranakther@users.noreply.github.com>
2025-03-19 17:27:25 +0530

    Home page creation

--- bakebuddy/frontend/src/App.jsx
+++ bakebuddy/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import '../css/App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import SalesCreate from './pages/SalesCreate';
+import Home from './pages/Home'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return <BrowserRouter>
     <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='/create-sales' element={<SalesCreate/>}/>
     </Routes>
     </BrowserRouter>
