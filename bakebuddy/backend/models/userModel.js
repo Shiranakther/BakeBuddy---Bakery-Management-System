d3840361aa494339f26e3f928f6becb110710ad4
DCKumara07 <159523598+DCKumara07@users.noreply.github.com>
2025-04-11 12:55:27 +0530

    user model re make

--- bakebuddy/backend/models/userModel.js
+++ bakebuddy/backend/models/userModel.js
@@ -2,16 +2,39 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    companyName: { type: String, required: false },
-    email: { type: String, required: true, unique: true },
-    phoneNumber: { type: String, required: false },
-    address: { type: String, required: false },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'supervisor'], default: 'supervisor' },
-  },
-  { timestamps: true }
+    firstName: { 
+      type: String, 
+      required: true 
+    },
+    lastName: { 
+      type: String, 
+      required: true 
+    },
+    companyName: { 
+      type: String, 
+      required: false 
+    },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
+    phoneNumber: { 
+      type: String, 
+      required: false 
+    },
+    address: { 
+      type: String, 
+      required: false 
+    },
+    password: { 
+      type: String, 
+      required: true 
+    },
+    role: { 
+      type: String, 
+      enum: ['admin', 'supervisor'], default: 'supervisor' 
+    },},{ timestamps: true }
 );
 
 // Optional: Add a virtual field for full name
