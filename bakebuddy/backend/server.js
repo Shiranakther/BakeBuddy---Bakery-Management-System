8090241de2779433603a8f6fd0386dc2875060ee
Shiran <127624730+Shiranakther@users.noreply.github.com>
2025-05-09 05:21:39 +0530

    Enable CORS with a wildcard origin in server configuration

--- bakebuddy/backend/server.js
+++ bakebuddy/backend/server.js
@@ -15,7 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+}));
 
 
 app.use("/api/production",productionRoutes);
